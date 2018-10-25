b4d3b7ebd08beb312eda85f37786b945cebef182
Iftekhar A Chowdhury <007ifte@gmail.com>
2018-10-25 15:17:55 -0400

    vue_directives

---
+++ Vue_directives/app.js
@@ -0,0 +1,24 @@
+const helloWorld = new Vue({
+    
+    el: "#helloVue",
+    data: {
+        title: "Hello world, This is Iftekhar",
+        message: "This is my first message in the world of Vue !"
+        
+    }
+});
+
+const newExample = new Vue({
+    
+    el: "#newExample",
+    data: {
+        title: "Another message",
+        message: "msg ..... ",
+        name: "iftekhar a chowdhury",
+        img: {
+            src:"https://placeimg.com/200/200/animals",
+            alt: "A place holder image of animals"
+        }
+        
+    }
+});
